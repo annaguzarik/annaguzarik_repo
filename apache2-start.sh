1140c7c034520245c38ebd13b58aa71243a7f0eb
annaguzarik <131981968+annaguzarik@users.noreply.github.com>
2025-01-26 12:38:07 +0300

    Update apache2-start.sh

--- apache2-start.sh
+++ apache2-start.sh
@@ -3,8 +3,5 @@
 sudo apt-get update
 sudo apt-get install -y apache2
 
-# Create a default web page
-echo "<html><body><h1>Hello, Azure!</h1></body></html>" | sudo tee /var/www/html/index.html
-
 # Restart Apache
 sudo systemctl restart apache2
